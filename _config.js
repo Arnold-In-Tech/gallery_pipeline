ded09c7e3fa18efa24952ea490c60f3a60fb0360
Arnold-In-Tech <g16a7782@gmail.com>
2024-11-25 16:24:19 +0300

    updated config connection

--- _config.js
+++ _config.js
@@ -7,13 +7,9 @@ config.mongoURI = {
 //    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 
 // Updated connection string, username and password
-//    production: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
-//    development: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
-//    test: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
-
-   production: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/darkroom?retryWrites=true&w=majority',
-   development: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-   test: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+   production: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
+   development: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
+   test: 'mongodb+srv://arnold123:arnold123@arnoldscluster.njnnl.mongodb.net/?retryWrites=true&w=majority&appName=ArnoldsCluster',
 
 }
 module.exports = config;
